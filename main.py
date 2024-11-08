eec769a6ef02195956c6caf495d86620b070a0f8
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-11-08 22:39:54 +0200

    PowerOf8

---
+++ main.py
@@ -0,0 +1,14 @@
+def is_power_of_8(n):
+    if n < 1:
+        return False
+
+    while n % 8 == 0:
+        n //= 8  
+
+    return n == 1
+
+number = int(input("Enter your number: "))
+if is_power_of_8(number):
+    print(f"Yes, {number} is a power of 8.")
+else:
+    print(f"No, {number} is not a power of 8.")
